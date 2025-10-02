e374b9521949115592b7bd5841a92bd32e5fb39a
kaming325 <chengkaming2012@gmail.com>
2025-10-01 22:39:55 -0400

    add get endpoint

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,22 @@ app.post('/api/expense', async (c) => {
   return c.text('Expense record created' )
 })
 
+app.get('/api/expense', async (c) => {
+  c.header('Access-Control-Allow-Origin', '*');
+  const [startDate, endDate] = [c.req.query('startDate'), c.req.query('endDate')];
+  if(!startDate || !endDate) {
+    return c.text('Missing startDate or endDate!')
+  }
+  const stmt = c.env?.EXPENSES_DB.prepare("SELECT * FROM Expense WHERE create_dateTime BETWEEN ? AND ?").bind(startDate, endDate);
+  const expenses = await stmt.run();
+  return c.json(expenses.results)
+})
+//   const stmt = c.env?.EXPENSES_DB.prepare("INSERT INTO Expense (merchant, amount, create_dateTime) VALUES (?, ?, ?)").bind(merchant, amount, new Date().toISOString());
+//   const res = await stmt.run();
+//   if (!res.success) {
+//     return c.text("Failed to create expense record")
+//   }
+//   return c.text('Expense record created' )
+// })
+
 export default app
